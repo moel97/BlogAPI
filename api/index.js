babe61c7eec30791faa7f1e1a57ce9e6a027dc81
Moel <mohamedalkasaby1@gmail.com>
2025-01-22 12:37:08 +0100

    corsOptions was not right

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ import multer from 'multer'
 const API = express();
 const port = 3000;
 const corsOptions ={
-    origin: 'http://localhost:5173', 
+    origin: 'https://blogpost-r1z0nvxa6-moel97s-projects.vercel.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
