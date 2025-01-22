bf14fe926aed55d8d37af6ecd06baff397c37493
Moel <mohamedalkasaby1@gmail.com>
2025-01-22 16:54:47 +0100

    bug - upload func

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRouter from "./routes/auth.js"
 import usersRouter from "./routes/users.js"
 import cookieParser from 'cookie-parser'
 import multer from 'multer'
+import path from 'path'
 const API = express();
 const port = 3000;
 const corsOptions ={
@@ -18,33 +19,32 @@ API.use(express.json())
 API.use(cookieParser())
 
 API.get('/photo/:imageName', (req, res) => {
-  const photoPath = path.join( req.params.imageName); // Adjust path as needed
+  const photoPath = path.join(__dirname, req.params.imageName); // Adjust path as needed
   res.sendFile(photoPath);
 });
 
 // Multer handling photos upload
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './')
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now()+"_"+file.originalname )
-    }
-  })
-  
-  const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, './'); // Save files in the current directory
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "_" + file.originalname); // Prefix filename with timestamp
+  }
+});
 
+const upload = multer({ storage: storage });
 
-API.post('/api/upload',upload.single('photo'), function (req, res) {
-  console.log("tring to upload photo");
-try {
+API.post('/api/upload', upload.single('photo'), function (req, res) {
+  console.log("Trying to upload photo");
+  try {
     const fileUrl = `${req.protocol}://${req.get('host')}/photo/${req.file.filename}`;
     res.status(200).json(fileUrl);
   } catch (error) {
-    console.log("failed to upload photo");
+    console.log("Failed to upload photo", error);
     res.status(500).json({ message: 'Error uploading photo', error });
   }
-  })
+});
 
 
 API.use("/api/posts",postRouter)
