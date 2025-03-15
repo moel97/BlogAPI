b39440dc5924a42ebc6cec3936ab1cccb6bc136f
Mohamed Elsayed <mohamed.elsayed@asap.de>
2025-03-15 23:59:15 +0100

    new DB connection

--- api/db.js
+++ api/db.js
@@ -1,11 +1,13 @@
 import pkg from 'pg';
-const { Client } = pkg;
-let client = new Client({
+import pkg from 'pg';
+const { Pool } = pkg;
+let client = new Pool({
   user: process.env.DB_user, 
   host: process.env.DB_host, 
   database: process.env.DB_database, 
   password: process.env.DB_password, 
   port: process.env.DB_port,
+  pool_mode: 'session',
   ssl: {
     rejectUnauthorized: false // This is often required for self-signed certificates
     }
